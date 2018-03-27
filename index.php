931f1ccb7719ae12e7c8c7c70f9b83dbf7b0a3ab
Bernardjacques <bernardjacques789@gmail.com>
2018-03-27 16:41:20 +0200

    Css-at-80%

--- index.php
+++ index.php
@@ -18,123 +18,162 @@
             <h2>5 Reasons your dog is vomiting</h2> <!-- Remplacer par "Article à la une" -->
         </div>
         <div class="secondary_articles">
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <h3>top 14 fresh snacks for dogs</h3> <!-- Remplacer par "Articles Secondaire" -->
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <h3>why skinny dogs live longer</h3> <!-- Remplacer par "Articles Secondaire" -->
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <h3>The 10 most dangerous foods for dogs</h3> <!-- Remplacer par "Articles Secondaire" -->
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <h3>The history of commercial pet food: a great american marketing story</h3><!-- Remplacer par "Articles Secondaire" -->
+            <div>
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <h3>top 14 fresh snacks for dogs</h3> <!-- Remplacer par "Articles Secondaire" -->
+            </div>
+            <div>
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <h3>why skinny dogs live longer</h3> <!-- Remplacer par "Articles Secondaire" -->
+            </div>
+            <div>
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <h3>The 10 most dangerous foods for dogs</h3> <!-- Remplacer par "Articles Secondaire" -->
+            </div>
+            <div>
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <h3>The history of commercial pet food: a great american marketing story</h3><!-- Remplacer par "Articles Secondaire" -->
+            </div>
         </div>
     </section>
     <section class="featured_posts">
+        <h2>Featured posts</h2>
         <div class="post">    
             <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Featured     <!-- Import DB TAGS -->
-                </span>
-                <span class="tag"> 
-                    Nutrition   <!-- Import DB TAGS -->
-                </span>
+            <div class="post_details">        
+                <div class="tags">
+                    <span class="tag">
+                        Featured     <!-- Import DB TAGS -->
+                    </span>
+                    <span class="tag"> 
+                        Nutrition   <!-- Import DB TAGS -->
+                    </span>
+                </div>
+                <h3>Cooking for your dog? Watch out for the nutrient deficiencies</h3> <!-- Import DB Title -->
+                <p>So you've discovered the many issues with commercial pet food and have devided that a home cooked diet is much</p> <!-- Import DB Resume -->
+                <button type="submit" name="share">Share</button>
             </div>
-            <h2>Cooking for your dog? Watch out for the nutrient deficiencies</h2> <!-- Import DB Title -->
-            <p>So you've discovered the many issues with commercial pet food and have devided that a home cooked diet is much</p> <!-- Import DB Resume -->
-            <button type="submit" name="share"></button>
         </div>
         <div class="post">    
             <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Featured     <!-- Import DB TAGS -->
-                </span>
-                <span class="tag"> 
-                    Nutrition   <!-- Import DB TAGS -->
-                </span>
+            <div class="post_details">        
+                <div class="tags">
+                    <span class="tag">
+                        Featured     <!-- Import DB TAGS -->
+                    </span>
+                    <span class="tag"> 
+                        Nutrition   <!-- Import DB TAGS -->
+                    </span>
+                </div>
+                <h3>Cooking for your dog? Watch out for the nutrient deficiencies</h3> <!-- Import DB Title -->
+                <p>So you've discovered the many issues with commercial pet food and have devided that a home cooked diet is much</p> <!-- Import DB Resume -->
+                <button type="submit" name="share">Share</button>
             </div>
-            <h2>Cooking for your dog? Watch out for the nutrient deficiencies</h2> <!-- Import DB Title -->
-            <p>So you've discovered the many issues with commercial pet food and have devided that a home cooked diet is much</p> <!-- Import DB Resume -->
-            <button type="submit" name="share"></button>
         </div>
         <div class="post">    
             <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Featured     <!-- Import DB TAGS -->
-                </span>
-                <span class="tag"> 
-                    Nutrition   <!-- Import DB TAGS -->
-                </span>
+            <div class="post_details">        
+                <div class="tags">
+                    <span class="tag">
+                        Featured     <!-- Import DB TAGS -->
+                    </span>
+                    <span class="tag"> 
+                        Nutrition   <!-- Import DB TAGS -->
+                    </span>
+                </div>
+                <h3>Cooking for your dog? Watch out for the nutrient deficiencies</h3> <!-- Import DB Title -->
+                <p>So you've discovered the many issues with commercial pet food and have devided that a home cooked diet is much</p> <!-- Import DB Resume -->
+                <button type="submit" name="share">Share</button>
             </div>
-            <h2>Cooking for your dog? Watch out for the nutrient deficiencies</h2> <!-- Import DB Title -->
-            <p>So you've discovered the many issues with commercial pet food and have devided that a home cooked diet is much</p> <!-- Import DB Resume -->
-            <button type="submit" name="share"></button>
         </div>
     </section>
     <section class="latest_posts">
-        <div class="post">
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Nutrition     <!-- Import DB TAGS -->
-                </span>
-                <h2>The 11 dogs food label tricks every owner need to know</h2> <!-- Import DB Title -->
-                <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+        <h2>Latest Posts</h2>
+        <div class="row">
+            <div class="post">
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <div class="featured_details">
+                    <span class="tag">
+                        Nutrition     <!-- Import DB TAGS -->
+                    </span>
+                    <h3>The 11 dogs food label tricks every owner need to know</h3> <!-- Import DB Title -->
+                    <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+                </div>
+            </div>
+            <div class="post">
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <div class="featured_details">
+                    <span class="tag">
+                        Nutrition     <!-- Import DB TAGS -->
+                    </span>
+                    <h3>The 11 dogs food label tricks every owner need to know</h3> <!-- Import DB Title -->
+                    <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+                </div>
             </div>
         </div>
-        <div class="post">
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Nutrition     <!-- Import DB TAGS -->
-                </span>
-                <h2>The 11 dogs food label tricks every owner need to know</h2> <!-- Import DB Title -->
-                <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+        <div class="row">
+            <div class="post">
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <div class="featured_details">
+                    <span class="tag">
+                        Nutrition     <!-- Import DB TAGS -->
+                    </span>
+                    <h3>The 11 dogs food label tricks every owner need to know</h3> <!-- Import DB Title -->
+                    <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+                </div>
+            </div>
+            <div class="post">
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <div class="featured_details">
+                    <span class="tag">
+                        Nutrition     <!-- Import DB TAGS -->
+                    </span>
+                    <h3>The 11 dogs food label tricks every owner need to know</h3> <!-- Import DB Title -->
+                    <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+                </div>
             </div>
         </div>
-        <div class="post">
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Nutrition     <!-- Import DB TAGS -->
-                </span>
-                <h2>The 11 dogs food label tricks every owner need to know</h2> <!-- Import DB Title -->
-                <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+        <div class="row">
+            <div class="post">
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <div class="featured_details">
+                    <span class="tag">
+                        Nutrition     <!-- Import DB TAGS -->
+                    </span>
+                    <h3>The 11 dogs food label tricks every owner need to know</h3> <!-- Import DB Title -->
+                    <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+                </div>
             </div>
-        </div>
-        <div class="post">
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Nutrition     <!-- Import DB TAGS -->
-                </span>
-                <h2>The 11 dogs food label tricks every owner need to know</h2> <!-- Import DB Title -->
-                <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
-            </div>
-        </div>
-        <div class="post">
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Nutrition     <!-- Import DB TAGS -->
-                </span>
-                <h2>The 11 dogs food label tricks every owner need to know</h2> <!-- Import DB Title -->
-                <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
-            </div>
-        </div>
-        <div class="post">
-            <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
-            <div class="tags">
-                <span class="tag">
-                    Nutrition     <!-- Import DB TAGS -->
-                </span>
-                <h2>The 11 dogs food label tricks every owner need to know</h2> <!-- Import DB Title -->
-                <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+            <div class="post">
+                <img src="assets/images/image01.jpg" alt="article"> <!-- Import DB IMG -->
+                <div class="featured_details">
+                    <span class="tag">
+                        Nutrition     <!-- Import DB TAGS -->
+                    </span>
+                    <h3>The 11 dogs food label tricks every owner need to know</h3> <!-- Import DB Title -->
+                    <p>Health-conscious consumers are skilled at deciphering nutrition facts labels. We know to monitor our sodium intake, avoid carrageenan and high</p>
+                </div>
             </div>
         </div>
         </section>
-        <section class="ads">
+        <section class="right">
+            <!-- <div class="social">
+                <div class="fb">
+                    <img src="social_fb.jpg" alt="facebook">
+                    <span>154832 Fans</span>
+                    <button type="submit" name="like">LIKE</button>
+                </div>
+                <div class="twitter"> 
+                    <img src="social_twi.jpg" alt="twitter">
+                    <span>124738 Followers</span>
+                    <button type="submit" name="follow">follow</button>
+                </div>
+                <div class="instagram">
+                    <img src="social_insta.jpg" alt="instagram"> 
+                    <span>164985 Followers</span>
+                    <button type="submit" name="like">follow</button>
+                </div>
+            </div> -->
             <div class="side_ad">
             </div>
         </section>
@@ -143,7 +182,7 @@
 
 <?php
 //
-// ================================== Version - 0.0.3 ========================================
+// ================================== Version - 0.1.3 ========================================
 // ======= Starting - 26/03/18 =========================== Ending - 11/04/18 =================
 // ====================================== Author : ===========================================
 //

8c02c5572fff657e019eac7d722603894dd8109d
dmshd <daniel.muyshond@gmail.com>
2018-03-28 14:52:01 +0200

    improved functions.php & header.php + integration

--- header.php
+++ header.php
@@ -10,16 +10,39 @@
 
     <link rel="stylesheet" href="<?= bloginfo('stylesheet_url'); ?>"  type="text/css" media="screen">
 
-    <!-- Team six - Daniel - Appel de la function load_font définie dans functions.php (Google Fonts) -->
-    <?php add_action('wp_enqueue_scripts', 'load_fonts'); ?>
-
-    <!-- Team six - Daniel - Appel de la function theme_js définie dans functions.php (bootstrap.min.js) -->
-    <?php add_action('wp_enqueue_scripts', 'theme_js'); ?>
-
-    <!-- Team six - Daniel - Appel de la function theme_styles définie dans functions.php (bootstrap.min.css) -->
-    <?php add_action('wp_enqueue_scripts', 'theme_styles'); ?>
-
+    <!-- Team six - Loading Scripts (voir teamSixScripts() dans functions.php -->
+    <?php add_action('wp_enqueue_scripts', 'teamSixScripts'); ?>
+    <!--Font Awesome integration script -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 
     <?php wp_head(); ?>
 </head>
 <body>
+<header>
+    <div class="header-social">
+        <div class="logo-image">
+            <img src="logo.png" alt="Digest">
+        </div>
+
+        <div class="social-network">
+
+            <ul class="favicons">
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Instagram</li>
+                <li>Search</li>
+            </ul>
+
+        </div>
+    </div>
+
+    <div class="navbar">
+        <ul class="text-center">
+            <li><a href="">NUTRITION</a></li>
+            <li><a href="">WELLNESS</a></li>
+            <li><a href="">LIFESTYLE</a></li>
+            <li><a href="">COMMUNITY</a></li>
+        </ul>
+    </div>
+
+</header>

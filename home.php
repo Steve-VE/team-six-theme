e14886e23d6bc6837a5ac3e1a0f8bb859e964868
gitmanfel <felicien.manzi@gmail.com>
2018-03-29 16:20:55 +0200

    article post

---
+++ home.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+HOME
+<ul>
+<?php
+$args = array( 'numberposts' => 5, 'order'=> 'ASC', 'orderby' => 'title' );
+$postslist = get_posts( $args );
+foreach ($postslist as $post) :  setup_postdata($post); ?> 
+
+		<li><?php the_title(); ?></li>
+        <li><?php the_excerpt(); ?></li>
+        <li><?php the_date(); ?></li>
+        <li><?php the_category(); ?></li>
+<?php endforeach; ?>
+</ul>
+    
+</body>
+</html>
+
+<?php
+get_footer();
+//
+// ================================== Version - 0.1.3 ========================================
+// ======= Starting - 26/03/18 =========================== Ending - 11/04/18 =================
+// ====================================== Author : ===========================================
+//
+//  MMP""MM""YMM `7MM"""YMM        db      `7MMM.     ,MMF'      .M"""bgd `7MMF'`YMM'   `MP' 
+//  P'   MM   `7   MM    `7       ;MM:       MMMb    dPMM       ,MI    "Y   MM    VMb.  ,P   
+//       MM        MM   d        ,V^MM.      M YM   ,M MM       `MMb.       MM     `MM.M'    
+//       MM        MMmmMM       ,M  `MM      M  Mb  M' MM         `YMMNq.   MM       MMb     
+//       MM        MM   Y  ,    AbmmmqMA     M  YM.P'  MM mmmmm .     `MM   MM     ,M'`Mb.   
+//       MM        MM     ,M   A'     VML    M  `YM'   MM       Mb     dM   MM    ,P   `MM.  
+//     .JMML.    .JMMmmmmMMM .AMA.   .AMMA..JML. `'  .JMML.     P"Ybmmd"  .JMML..MM:.  .:MMa.
+?>

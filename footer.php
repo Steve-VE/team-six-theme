702c61656f7a215292b91fb7ebd6813e60e2ab3c
Steve-VE <steve.vanessche@gmail.com>
2018-03-29 16:22:51 +0200

    Modif du footer

--- footer.php
+++ footer.php
@@ -4,25 +4,12 @@
                 <div class="col-md-4">
                     <h3>Categories</h3>
                     <aside class="categories">
-                        <ul>
-                            <!-- target="_blank" for opening a new tab, delete it to stay in the site 
-                          target="_self" for staying in the tab.-->
-                            <li>
-                                <a rel="noopener" href="http://www.becode.org/" target="_blank">Community</a>
-                            </li>
-                            <li>
-                                <a rel="noopener" href="http://www.becode.org/" target="_blank">Featured</a>
-                            </li>
-                            <li>
-                                <a rel="noopener" href="http://www.becode.org/" target="_parent">Lifestyle</a>
-                            </li>
-                            <li>
-                                <a rel="noopener" href="http://www.becode.org/" target="_self">Nutrition</a>
-                            </li>
-                            <li>
-                                <a rel="noopener" href="http://www.becode.org/">Wellness</a>
-                            </li>
-                        </ul>
+                        <?php wp_list_categories(array(
+                            'title_li' => '',
+                            'orderby'    => 'id',
+                            'include' => array( 2, 3, 4, 5, 6 ),
+                            'hide_empty' => false
+                        )); ?>
                     </aside>
                 </div>
 
@@ -41,14 +28,17 @@
             </div>
 
             <div class="container-responsive-2">
-                <div class="col-md-11">
+            <?php wp_nav_menu(array(
+                'menu' => 'mainheader_social_menu'
+            )); ?>
+                <!-- <div class="col-md-11">
                     <a href="#" class="fa fa-facebook"></a>
 
                     <a href="#" class="fa fa-twitter"></a>
 
                     <a href="#" class="fa fa-instagram"></a>
                 </div>
-            </div>
+            </div> -->
         </div>
     </footer>
 <?php wp_footer(); ?> <!-- Wordpress doc dit de placer ça juste avant le </body> -->

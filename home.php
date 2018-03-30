1e19819c3d860e246c19d3f62574fd1caa2942be
dmshd <daniel.muyshond@gmail.com>
2018-03-30 11:46:50 +0200

    functions, sidebar

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: HOME.PHP
+*/
+get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+<?php
+get_footer();
+?>
---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Default sidebar template
+*/
+?>
+<h2>sidebar.php</h2>
+<div id="sidebar-social-plus-ad" class="sidebar">
+    <?php dynamic_sidebar( 'social-plus-ad' ); ?>
+</div>

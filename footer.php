1846484f199fbefcdd1c9ccb8438f07f152a94ab
Steve-VE <steve.vanessche@gmail.com>
2018-03-30 11:45:55 +0200

    Modif de l'header et du footer, ainsi que du css associé

--- footer.php
+++ footer.php
@@ -1,46 +1,41 @@
 <footer>
-        <div class="container-responsive">
-            <div class="row">
-                <div class="col-md-4">
-                    <h3>Categories</h3>
-                    <aside class="categories">
-                        <?php wp_list_categories(array(
-                            'title_li' => '',
-                            'orderby'    => 'id',
-                            'include' => array( 2, 3, 4, 5, 6 ),
-                            'hide_empty' => false
-                        )); ?>
-                    </aside>
-                </div>
-
-
-                <div class="col-md-4">
-                    <h3>Popular Posts</h3>
-
-                </div>
-
-
-                <div class="col-md-4">
-                    <aside class="Instagram">
-                        <h3>Instagram</h3>
-                    </aside>
-                </div>
+    <div class="container-responsive">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Categories</h3>
+                <aside class="categories">
+                    <?php wp_list_categories(array(
+                        'title_li' => '',
+                        'orderby'    => 'id',
+                        'include' => array( 2, 3, 4, 5, 6 ),
+                        'hide_empty' => false
+                    )); ?>
+                </aside>
             </div>
 
-            <div class="container-responsive-2">
-            <?php wp_nav_menu(array(
-                'menu' => 'mainheader_social_menu'
-            )); ?>
-                <!-- <div class="col-md-11">
-                    <a href="#" class="fa fa-facebook"></a>
 
-                    <a href="#" class="fa fa-twitter"></a>
+            <div class="col-md-4">
+                <h3>Popular Posts</h3>
 
-                    <a href="#" class="fa fa-instagram"></a>
-                </div>
-            </div> -->
+            </div>
+
+
+            <div class="col-md-4">
+                <aside class="Instagram">
+                    <h3>Instagram</h3>
+                </aside>
+            </div>
         </div>
-    </footer>
+
+        <div class="container-responsive-2">
+        <?php wp_nav_menu(array(
+            'menu' => 'mainheader_social_menu',
+            'menu_class' => 'social-link'
+        )); ?>
+    </div>
+
+    <div id="button-scroll-up" onclick="alert('ok');"></div>
+</footer>
 <?php wp_footer(); ?> <!-- Wordpress doc dit de placer ça juste avant le </body> -->
 </body>
 

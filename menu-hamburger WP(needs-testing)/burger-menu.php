04633cdad83041bd328a5842296b2bb6c44bc0e7
PHennaux115 <phennaux115@gmail.com>
2018-04-02 13:20:56 +0200

    WP Burger(king)menu needs testing

---
+++ menu-hamburger WP(needs-testing)/burger-menu.php
@@ -0,0 +1,7 @@
+<?php
+    wp_nav_menu(array('container_class' => 'main-nav', 'theme-location' => 'primary')); 
+?>
+
+<link rel="stylesheet" type="text/css" href="assets/css/burger-menu.css">
+
+<a class="toggle-nav" href="#">&#9776;</a>

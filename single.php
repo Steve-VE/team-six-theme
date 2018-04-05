bb53ba72afdb7d1ebf16779c66e1ffd958872ee7
dmshd <daniel.muyshond@gmail.com>
2018-04-06 00:28:34 +0200

    maj single.php jeudi soir

--- single.php
+++ single.php
@@ -37,7 +37,13 @@ $post_thumbnail_param = array (
 
                     <div class="orange-circle"><p><?php bloginfo('title'); ?></p></div>
 
-                    <p class="post-author"><span class="by">by :</span> <?php the_author_posts_link(); ?></p>
+                    <p class="post-author">
+                    <span class="by">by :</span>
+                    <?php
+                    $author = get_the_author_meta('ID');
+                    echo get_avatar($author, $size = "48");
+                    the_author_posts_link(); 
+                    ?></p>
                     <a href="#"><button class="comment-button"><i class="fa fa-comment"></i>Comment</button></a>
 
                 </div>
@@ -50,6 +56,19 @@ $post_thumbnail_param = array (
                 <h5>Subscribe to The FooDog Newletter</h5>
                 <h6>Get health and wellness tips about your dog delivered to your inbox.</h6>
                 <input type="text" name="newletter_register_mail" placeholder="Your email"><a href="#"><button type="submit" value="newletter_register_submit">Sign up</button></a>
+            </aside>
+            <nav class="previous row">
+            <div class="previous-left col"><p>< Previous article</p></div>
+            <div class="previous-right col"></div>
+            </nav>
+            <aside class="about row">
+            <div class="col-sm-3">
+                <div class="medium-orange-circle"><p><?php bloginfo('title'); ?></p></div>
+            </div>
+            <div class="col-sm-7">
+                <p>FooDog is the leading direct-to-consumer, fresh pet food company, offering customers and their pets the highest quality and conveience without retail markups. All human-grade meal plans are made to order, designed by veterinarians, and personalized to provide the ideal nutritional balance for every dog. Get started today at</p>
+                <p><a href="<?php echo get_home_url(); ?>#">https://www.foodog.com/</a></p>
+            </div>
             </aside>
             <div class="post-comments">
                 <?php comments_template(); ?>

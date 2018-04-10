44f585036a9c21a9670ac5df891e5441aa0ebbf8
Bernardjacques <bernardjacques789@gmail.com>
2018-04-10 16:35:32 +0200

    addModalSearch

--- template-parts/menu-hamburger.php
+++ template-parts/menu-hamburger.php
@@ -3,6 +3,7 @@
  * Template Name: menu-hamburger
  */
 ?>
+<?php add_thickbox(); ?>
 <nav role="navigation">
   <div class="container-responsive">
     <div id="menuToggle">
@@ -22,4 +23,18 @@
       </ul>
     </div>
   </div>
+  <div id="my-content-id" style="display:none;">
+    <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+      <label>
+      <span class="screen-reader-text"></span>
+      <input type="search" class="search-field"
+      placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+      value="<?php echo get_search_query() ?>" name="s"
+      title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+      </label>
+      <input type="submit" class="search-submit"
+      value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+    </form>
+  </div>
+  <a href="#TB_inline?width=600&height=300&inlineId=my-content-id" class="thickbox">Search</a>
 </nav>
